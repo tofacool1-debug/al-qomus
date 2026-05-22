8ac45890956259d1dc4989a8dc992cd4c978a13c
tofacool1-debug <tofacool1@gmail.com>
2026-05-22 23:14:50 +0700

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "alqomus-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./sw.js",
+  "./data.js",  //
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
