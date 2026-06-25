0d4b24203c7ba032f734842365bc169980cb7b0a
tofacool1-debug <tofacool1@gmail.com>
2026-06-25 15:45:01 +0700

    Update import for SifatMusyabihatPluralTable

--- src/utils/sifatMusyabihatPlural.ts
+++ src/utils/sifatMusyabihatPlural.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { DictionaryEntry, SifatMusyabihatTable } from "../types";
+import { DictionaryEntry, SifatMusyabihatPluralTable } from "../types";
 
 type BinaKey = "sahih" | "ajwaf" | "mitsal" | "naqish" | "mudaaf" | "mahmuz" | "lafif_maqrun" | "lafif_mafruq" | "lafif";
 
